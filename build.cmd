da4e95d10dc2c8aae928db48e86a742abcb64a22
chris nurse <chris.nurse@namsource.com>
2021-12-21 17:31:25 +0800

    Setup multi-arch buld command

--- build.cmd
+++ build.cmd
@@ -1,1 +1,4 @@
+# Execute the following command once to enabled docker buildx to create multi-arch images
+# docker buildx create --name docker-builder --use
+
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag namsource/qiktrak:latest .
