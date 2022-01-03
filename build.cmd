7c0cdb7abf22e2841acfb1e27e8f054dc7744438
chris nurse <chris.nurse@namsource.com>
2022-01-03 12:43:20 +0800

    Multi architecture builder

--- build.cmd
+++ build.cmd
@@ -1,4 +1,7 @@
-# Execute the following command once to enabled docker buildx to create multi-arch images
+# NOTE! : Execute the following command once to enabled docker buildx to create multi-architecture images
+# 
 # docker buildx create --name docker-builder --use
+#
 
+npm install
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag namsource/qiktrak:latest .
